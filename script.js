f52060f42c84ae9b48b5240d6708eb070f00b226
Atokolo John <atokoloj@gmail.com>
2025-06-14 21:21:21 +0100

    debugged code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', function(){
 
     const form = document.getElementById('registration-form');
     const feedbackDiv = document.getElementById('form-feedback');
